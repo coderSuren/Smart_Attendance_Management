69f8dcc700e63c2cafd0dc58e0c6a58c615eec5e
Tarun R <75297835+rtarun9@users.noreply.github.com>
2023-05-09 20:01:54 +0530

    Varying body of page based on selected option

--- src/components/dashboard/Admin.js
+++ src/components/dashboard/Admin.js
@@ -17,6 +17,9 @@ const viewDbOptions = ['View Students', 'View Faculty', 'View Courses'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 const createEntryOptions = ['Create Student', 'Create Class', 'Create Faculty', 'Assign student to class'];
 
+var currentPage = pages[0];
+var selectQuery = false;
+
 function ResponsiveAppBar() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
@@ -46,172 +49,203 @@ function ResponsiveAppBar() {
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
   };
-  const handleCloseUserMenuDb = () => {
+  const handleCloseUserMenuDb = (option) => {
+    console.log(option);
+    selectQuery = false;
+    currentPage = option;
     setAnchorElUserViewDB(null);
   };
-  const handleCloseUserMenuCreateEntry = () => {
+  const handleCloseUserMenuCreateEntry = (option) => {
+    console.log(option);
+    selectQuery = false;
+    currentPage = option;
     setAnchorElUserCreateEntry(null);
   };
-  const handleCloseUserMenuQuery = () => {
+  const handleCloseUserMenuQuery = (option) => {
+    currentPage = option;
+    selectQuery = true;
+    console.log(option);
     setAnchorElUserQuery(null);
   };
 
   return (
     <>
-    <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.0rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            Smart Attendance System
+      <AppBar position="static">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="/"
+              sx={{
+                mr: 2,
+                display: { xs: 'none', md: 'flex' },
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.0rem',
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+            >
+              Smart Attendance System
+            </Typography>
+
+            <Box sx={{ flexGrow: 1, display: 'flex' }}>
+
+              <Button onClick={handleOpenUserMenuViewDB} sx={{ my: 2, color: 'white', display: 'block' }}>
+                View Database
+              </Button>
+
+              <Menu
+                sx={{ mt: '45px' }}
+                id="menu-appbar"
+                anchorEl={anchorElUserViewDB}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorElUserViewDB)}
+                onClose={handleCloseUserMenuDb}
+              >
+                {viewDbOptions.map((viewDbOption) => (
+                  <MenuItem key={viewDbOption} onClick={() => handleCloseUserMenuDb(viewDbOption)}>
+                    <Typography textAlign="center">{viewDbOption}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+
+              <Button onClick={handleOpenUserMenuCreateEntry} sx={{ my: 2, color: 'white', display: 'block' }}>
+                Create new Entry
+              </Button>
+
+              <Menu
+                sx={{ mt: '45px' }}
+                id="menu-appbar"
+                anchorEl={anchorElUserCreateEntry}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorElUserCreateEntry)}
+                onClose={handleCloseUserMenuCreateEntry}
+              >
+                {createEntryOptions.map((createEntry) => (
+                  <MenuItem key={createEntry} onClick={() => handleCloseUserMenuCreateEntry(createEntry)}>
+                    <Typography textAlign="center">{createEntry}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+
+              <Button
+                key={pages[2]}
+                onClick={() => {
+                  selectQuery = true;
+                }}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {pages[2]}
+              </Button>
+            </Box>
+
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: '45px' }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting) => (
+                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                    <Typography textAlign="center">{setting}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      { (currentPage === "View Students") && 
+        <Box component="main" sx={{ p: 3 }}>
+          <Typography>
+            View Students
           </Typography>
-
-          <Box sx={{ flexGrow: 1, display: 'flex' }}>
-
-            <Button onClick={handleOpenUserMenuViewDB} sx={{ my: 2, color: 'white', display: 'block' }}>
-              View Database
-            </Button>
-
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUserViewDB}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUserViewDB)}
-              onClose={handleCloseUserMenuDb}
-            >
-              {viewDbOptions.map((viewDbOption) => (
-                <MenuItem key={viewDbOption} onClick={handleCloseUserMenuDb}>
-                  <Typography textAlign="center">{viewDbOption}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-
-            <Button onClick={handleOpenUserMenuCreateEntry} sx={{ my: 2, color: 'white', display: 'block' }}>
-              Create new Entry
-            </Button>
-
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUserCreateEntry}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUserCreateEntry)}
-              onClose={handleCloseUserMenuCreateEntry}
-            >
-              {createEntryOptions.map((createEntry) => (
-                <MenuItem key={createEntry} onClick={handleCloseUserMenuCreateEntry}>
-                  <Typography textAlign="center">{createEntry}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-
-            <Button
-              key={pages[2]}
-              onClick={handleOpenUserMenuQuery}
-              sx={{ my: 2, color: 'white', display: 'block' }}
-            >
-              {pages[2]}
-            </Button>
-          </Box>
-          
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
-    <Box component="main" sx={{ p: 3 }} open={Boolean(anchorElUserQuery)}>
-        <Typography>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique unde
-          fugit veniam eius, perspiciatis sunt? Corporis qui ducimus quibusdam,
-          aliquam dolore excepturi quae. Distinctio enim at eligendi perferendis in
-          cum quibusdam sed quae, accusantium et aperiam? Quod itaque exercitationem,
-          at ab sequi qui modi delectus quia corrupti alias distinctio nostrum.
-          Minima ex dolor modi inventore sapiente necessitatibus aliquam fuga et. Sed
-          numquam quibusdam at officia sapiente porro maxime corrupti perspiciatis
-          asperiores, exercitationem eius nostrum consequuntur iure aliquam itaque,
-          assumenda et! Quibusdam temporibus beatae doloremque voluptatum doloribus
-          soluta accusamus porro reprehenderit eos inventore facere, fugit, molestiae
-          ab officiis illo voluptates recusandae. Vel dolor nobis eius, ratione atque
-          soluta, aliquam fugit qui iste architecto perspiciatis. Nobis, voluptatem!
-          Cumque, eligendi unde aliquid minus quis sit debitis obcaecati error,
-          delectus quo eius exercitationem tempore. Delectus sapiente, provident
-          corporis dolorum quibusdam aut beatae repellendus est labore quisquam
-          praesentium repudiandae non vel laboriosam quo ab perferendis velit ipsa
-          deleniti modi! Ipsam, illo quod. Nesciunt commodi nihil corrupti cum non
-          fugiat praesentium doloremque architecto laborum aliquid. Quae, maxime
-          recusandae? Eveniet dolore molestiae dicta blanditiis est expedita eius
-          debitis cupiditate porro sed aspernatur quidem, repellat nihil quasi
-          praesentium quia eos, quibusdam provident. Incidunt tempore vel placeat
-          voluptate iure labore, repellendus beatae quia unde est aliquid dolor
-          molestias libero. Reiciendis similique exercitationem consequatur, nobis
-          placeat illo laudantium! Enim perferendis nulla soluta magni error,
-          provident repellat similique cupiditate ipsam, et tempore cumque quod! Qui,
-          iure suscipit tempora unde rerum autem saepe nisi vel cupiditate iusto.
-          Illum, corrupti? Fugiat quidem accusantium nulla. Aliquid inventore commodi
-          reprehenderit rerum reiciendis! Quidem alias repudiandae eaque eveniet
-          cumque nihil aliquam in expedita, impedit quas ipsum nesciunt ipsa ullam
-          consequuntur dignissimos numquam at nisi porro a, quaerat rem repellendus.
-          Voluptates perspiciatis, in pariatur impedit, nam facilis libero dolorem
-          dolores sunt inventore perferendis, aut sapiente modi nesciunt.
-        </Typography>
-      </Box>
+        </Box>
+      }
+      { (currentPage === "View Faculty") && 
+        <Box component="main" sx={{ p: 3 }}>
+          <Typography>
+            View Faculty
+          </Typography>
+        </Box>
+      }
+      { (currentPage === "View Courses") && 
+        <Box component="main" sx={{ p: 3 }}>
+          <Typography>
+            View Courses
+          </Typography>
+        </Box>
+      }
+      { (selectQuery) && 
+        <Box component="main" sx={{ p: 3 }}>
+          <Typography>
+            Query Database
+          </Typography>
+        </Box>
+      }
+      { (currentPage === "Create Student") && 
+        <Box component="main" sx={{ p: 3 }}>
+          <Typography>
+            Create Student
+          </Typography>
+        </Box>
+      }
+      { (currentPage === "Create Class") && 
+        <Box component="main" sx={{ p: 3 }}>
+          <Typography>
+            Create Class
+          </Typography>
+        </Box>
+      }
+      { (currentPage === "Create Faculty") && 
+        <Box component="main" sx={{ p: 3 }}>
+          <Typography>
+            Create Faculty
+          </Typography>
+        </Box>
+      }
+      { (currentPage === "Assign student to class") && 
+        <Box component="main" sx={{ p: 3 }}>
+          <Typography>
+          Assign student to class
+          </Typography>
+        </Box>
+      }
     </>
   );
 }
